76dbb88993f79f57b6cde5931f47cace44115e2e
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-11-27 19:53:49 +0100

    Setup env

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,50 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    artifacts: "../client/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      accounts: {
+        mnemonic: process.env.WALLET_DEV_MNEMONIC,
+        accountsBalance: "1000000000000000000000",
+        count: 10,
+      },
+    },
+    goerli: {
+      url: process.env.GOERLI_ENDPOINT,
+      accounts: {
+        mnemonic: process.env.WALLET_DEV_MNEMONIC,
+        accountsBalance: "1000000000000000000000",
+        count: 10,
+      },
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_KEY as string,
+    },
+  },
 };
 
 export default config;
---
+++ hardhat/types/env.d.ts
@@ -0,0 +1,5 @@
+declare module "@env" {
+  export const WALLET_DEV_MNEMONIC: string;
+  export const GOERLI_ENDPOINT: string;
+  export const ETHERSCAN_KEY: string;
+}
